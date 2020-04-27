bf8a183827e2f237e582094601fcf222e254fbb0
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-04-26 23:40:56 -0300

    Adding restaurants service http

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -16,7 +16,9 @@ export class RestaurantsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.restaurants = this.restaurantsService.restaurants();
+    this.restaurantsService
+      .restaurants()
+      .subscribe(restaurants => this.restaurants = restaurants);
   }
 
 }
