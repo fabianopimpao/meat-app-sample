508a61f5c956ac43385e27fa2523b2ea45d1973d
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-05-13 06:55:27 -0300

    Providing purchase items

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -14,7 +14,7 @@ export class ShoppingCartService {
     addItem(item: MenuItem) {
         let foundItem = this.items.find((miItem) => miItem.menuItem.id === item.id);
         if (foundItem) {
-            foundItem.quantity += 1;
+            this.increaseQty(foundItem);
         } else {
             this.items.push(new CartItem(item));
         }
@@ -28,5 +28,16 @@ export class ShoppingCartService {
         return this.items
             .map(item => item.value())
             .reduce((prev, value) => prev + value, 0);            
+    }
+
+    increaseQty(item: CartItem) {
+        item.quantity = item.quantity + 1;
+    }
+
+    decreaseQty(item: CartItem) {
+        item.quantity = item.quantity - 1;
+        if (item.quantity === 0) {
+            this.removerItem(item);
+        }
     }
 }
